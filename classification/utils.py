c57149703b503c08ca63711e68174fe6737e035a
GwonilJoo <gwonil3256@gmail.com>
2022-03-09 03:30:03 +0900

    Update utils.py

--- classification/utils.py
+++ classification/utils.py
@@ -5,12 +5,29 @@ import gc
 import matplotlib.pyplot as plt
 from tqdm import tqdm
 
+names = ('Center', 'Donut', 'Edge-Loc', 'Edge-Ring', 'Loc', 'Near-full', 'Random', 'Scratch', 'none')
+
+def oneHotEncoding(label):
+    one_hot_vector = [[0] * len(names) for _ in range(len(label))]
+    for i in range(len(label)):
+        index = names.index(label[i][0])
+        one_hot_vector[i][index] = 1
+    one_hot_vector = np.array(one_hot_vector, dtype=np.float32)
+    return one_hot_vector
+    
+
 def loadDataset(data_set, label_set):
     data_df = pd.read_pickle(data_set)
     label_df = pd.read_pickle(label_set)
 
-    data = np.array(data_df, dtype=np.float32)
+    data = np.array(data_df)
     label = np.array(label_df)
+    
+    label = np.squeeze(label)
+    for i, name in enumerate(names):
+        label[label == name] = i
+    label = np.array(label, dtype=np.int64)
+    # label = oneHotEncoding(label)
 
     return data, label
 
@@ -21,9 +38,14 @@ if __name__ == "__main__":
 
     wafer, label = loadDataset(wafer_set, label_set)
 
-    cases = np.unique(label)
-    for c in cases:
-        print(f'{c}: {len(label[label == c])}')
+    print(wafer.shape)
+    print(label.shape)
+    print(label[0])
+    print(type(label[0]))
 
-    gen_wafer = np.argmax(wafer, axis=3)
-    plt.imsave(f'{label[0][0]}.png', gen_wafer[0])
+    # cases = np.unique(label)
+    # for c in cases:
+    #     print(f'{c}: {len(label[label == c])}')
+
+    # gen_wafer = np.argmax(wafer, axis=3)
+    # plt.imsave(f'{label[0][0]}.png', gen_wafer[0])
